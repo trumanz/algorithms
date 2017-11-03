b57a6badab260fa135b661dfb1cb30d7ab00d689
changkai <changkai.zhou@youhualin.com>
2017-11-03 11:42:20 +0800

    deV

--- install-dependency.sh
+++ install-dependency.sh
@@ -3,4 +3,4 @@ sudo apt-get install git bison flex python-setuptools python-dev autoconf \
 libtool zlib1g-dev texinfo help2man gawk g++ curl texlive subversion && \
 wget https://github.com/google/or-tools/archive/v6.4.tar.gz && \
 tar xf v6.4.tar.gz && \
-cd or-tools-6.4 && make third_party  
+cd or-tools-6.4 && make third_party   && make cc
