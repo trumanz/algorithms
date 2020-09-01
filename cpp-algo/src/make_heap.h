cd63afeb5a76c1177d9140b4b876baba663b2e7d
Truman Zhou <truman.ck.zhou@gmail.com>
2020-09-01 23:13:09 +0800

    chg: add build head by adjust

--- cpp-algo/src/make_heap.h
+++ cpp-algo/src/make_heap.h
@@ -1,3 +1,4 @@
+#include <iostream>
 
 /*
     0    
@@ -11,6 +12,10 @@ parent  arr[(i-1)/2]
 class HeapTool{
 
 public:
+    bool debug_output;
+    HeapTool(){
+        debug_output = false;
+    }
     void recusiveBuildHeapOfNode(int arr[], int len, int parenet_index)
     {
        int left_child_index = 2 * parenet_index + 1;
@@ -39,20 +44,79 @@ public:
     }
 
     void adjustHeap(int arr[], int len, int hole_index, int value) {
-        //adjust heap, so that we have hole of last node; mode  the hole_index to suitable position and then push value;
-
-        //subtree of hole_index are all complete tree;
-
-        for(int parent_index = hole_index; parent_index < (len-1)/2; parent_index++) {
-
+        //adjust heap, so that we have hole of last node; mode  the hole_index to suitable position and then push value
+        const int top_index = hole_index;
+        //assume subtree of hole_index are all complete tree;
+        //now we delete data which located in hole_index,
+        // (len-1)/2 , so those parent node have both left and right nodes;
+        for(int parent_index = hole_index; parent_index < (len-1)/2; ) {
+             int left_child_index  =  2*parent_index+1; 
+             int right_child_index = left_child_index + 1; 
+             if(arr[left_child_index] > arr[right_child_index]) { 
+                  arr[hole_index] = arr[left_child_index];
+                  hole_index = left_child_index;
+             } else {
+                  arr[hole_index] = arr[right_child_index];
+                  hole_index = right_child_index;
+             }
+             parent_index = hole_index;
         }
+        //last node with onle one leaf
+        if( (len&1) == 0 && hole_index == (len-2)/2) {
+             int left_child_index  =  2*hole_index+1;
+             arr[hole_index] = arr[left_child_index];
+             hole_index = left_child_index;
+        }
+        // and push new value to tree
+        
+        pushHeap(arr, len, top_index, hole_index, value);
+        
         
     } 
+    void pushHeap(int arr[], int len, int top_index, int hole_index, int value) {
+        if(debug_output) {
+            outArray(arr, len);
+            std::cout <<"\n hold_index:" << hole_index <<  ", value:" << value << ", top_index:" << top_index << "\n";
+        }
+        //compare ancestor by step, find a suitable hole_index
+        for(int parent_index = (hole_index-1)/2; hole_index > top_index ;) {
+            if(arr[parent_index] < value) {
+                arr[hole_index] = arr[parent_index];
+                hole_index = parent_index;
+                parent_index = (hole_index -1)/2;
+            } else {
+                break;
+            }
+        }
+        arr[hole_index] = value;
+    }
+
 
     void buildHeapByAdjust(int arr[], int len) {
+
+        if(debug_output) {
+             std::cout << "\nInput Arr: ";
+             for(int j = 0; j < len; j++) {
+                 std::cout << arr[j] <<",";
+             }
+        }
+
+
         for(int parent_node_index = (len-1-1)/2 ; parent_node_index >= 0; parent_node_index--){
            adjustHeap(arr, len, parent_node_index, arr[parent_node_index]);
+           if(debug_output) {
+             outArray(arr, len);
+             std::cout << "     parent_nodex_index=" << parent_node_index << "\n";
+          }
         }
     }
+ 
+    void outArray(int arr[], int len) {
+        std::cout << "\nAdjustArr: ";
+             for(int j = 0; j < len; j++) {
+                 std::cout << arr[j] <<",";
+        }
+    }
+    
 
 };
--- cpp-algo/src/make_heap_test.cpp
+++ cpp-algo/src/make_heap_test.cpp
@@ -1,7 +1,8 @@
 #include "make_heap.h"
 #include "gtest/gtest.h"
 #include <algorithm>
-
+#include <random>
+#include <iostream>
 
 TEST(buildHeap, heapify_array){
      
@@ -17,3 +18,55 @@ TEST(buildHeap, heapify_array){
     std::cout<<"\n";
     ASSERT_TRUE( std::is_heap(arr, arr + arr_len) );
 };
+TEST(buildHeap, buildHeapByAdjust){
+     
+    int arr[] = {0,1,2,3,4,5,6};
+    const int arr_len = sizeof(arr)/sizeof(arr[0]);
+    ASSERT_FALSE( std::is_heap(arr, arr + arr_len) );
+
+    HeapTool().buildHeapByAdjust(arr,arr_len );
+
+    for(int x : arr){
+        std::cout << x <<", "; 
+    }
+    std::cout<<"\n";
+    ASSERT_TRUE( std::is_heap(arr, arr + arr_len) );
+};
+
+TEST(buildHeap, randombuildHeap){
+
+
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> len_distr(0, 50);
+    std::uniform_int_distribution<> num_distr(0, 100);
+
+
+    for(size_t i = 0; i < 100000; i++) {
+        int len = len_distr(gen);
+        int* arr = (int*)malloc(len*sizeof(int));
+        int* arr_bak = (int*)malloc(len*sizeof(int));
+        for(int j = 0; j < len; j++) {
+            int n = num_distr(gen);
+            arr[j] = n;
+            arr_bak[j] = n;
+        }
+        if((i&1)== 1) HeapTool().buildHeapByAdjust(arr, len);
+        else HeapTool().recusiveBuildHeap(arr, len);
+
+        bool is_heap = std::is_heap(arr, arr + len);
+        if(is_heap == false ) {
+            HeapTool ht;
+            ht.debug_output = true;
+            ht.buildHeapByAdjust(arr_bak, len);
+        }
+        ASSERT_TRUE( std::is_heap(arr, arr + len) );
+        free(arr);
+        free(arr_bak);
+    }
+   
+};
+
+
+
+
